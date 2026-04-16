0ab42b1580a1882926fb1d0f0285bfea417a3601
abdalrhman <abdalrhmanmogahed2002@gmail.com>
2026-04-16 10:39:20 +0200

    migrate to postgresql

--- src/main/java/com/am9/okazx/OkazxApplication.java
+++ src/main/java/com/am9/okazx/OkazxApplication.java
@@ -2,12 +2,12 @@ package com.am9.okazx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class OkazxApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OkazxApplication.class, args);
 	}
-
 }
